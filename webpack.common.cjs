04a6ac53988b53fa81e91bf250d335ed65c88441
Traunin <traunin5@gmail.com>
2024-07-17 05:20:22 +0300

    chore: add webpack configs

---
+++ webpack.common.cjs
@@ -0,0 +1,35 @@
+const path = require("path")
+
+const CopyPlugin = require("copy-webpack-plugin")
+const MiniCssExtractPlugin = require("mini-css-extract-plugin")
+
+module.exports = {
+    entry: {
+        popup: "./src/popup.js",
+        service_worker: "./src/service_worker.js"
+    },
+
+    output: {
+        filename: '[name].js',
+        path: path.resolve(__dirname, "dist")
+    },
+
+    module: {
+        rules: [
+            {
+                test: /\.css&/,
+                use: [MiniCssExtractPlugin.loader, "css-loader"]
+            }
+        ]
+    },
+
+    plugins: [
+        new CopyPlugin({
+            patterns: [{ from: "static" }],
+        }),
+
+        new MiniCssExtractPlugin({
+            filename: "[name.css]",
+        })
+    ],
+}
