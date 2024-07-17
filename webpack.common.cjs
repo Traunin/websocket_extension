1efcbae7f48dccfc74cf6f7826bf5d4b133bdd5a
Traunin <traunin5@gmail.com>
2024-07-17 19:25:47 +0300

    style: run prettier

--- webpack.common.cjs
+++ webpack.common.cjs
@@ -1,35 +1,35 @@
-const path = require("path")
+const path = require("path");
 
-const CopyPlugin = require("copy-webpack-plugin")
-const MiniCssExtractPlugin = require("mini-css-extract-plugin")
+const CopyPlugin = require("copy-webpack-plugin");
+const MiniCssExtractPlugin = require("mini-css-extract-plugin");
 
 module.exports = {
-    entry: {
-        popup: "./src/popup.js",
-        service_worker: "./src/service_worker.js"
-    },
+  entry: {
+    popup: "./src/popup.js",
+    service_worker: "./src/service_worker.js",
+  },
 
-    output: {
-        filename: '[name].js',
-        path: path.resolve(__dirname, "dist")
-    },
+  output: {
+    filename: "[name].js",
+    path: path.resolve(__dirname, "dist"),
+  },
 
-    module: {
-        rules: [
-            {
-                test: /\.css&/,
-                use: [MiniCssExtractPlugin.loader, "css-loader"]
-            }
-        ]
-    },
-
-    plugins: [
-        new CopyPlugin({
-            patterns: [{ from: "static" }],
-        }),
-
-        new MiniCssExtractPlugin({
-            filename: "[name.css]",
-        })
+  module: {
+    rules: [
+      {
+        test: /\.css&/,
+        use: [MiniCssExtractPlugin.loader, "css-loader"],
+      },
     ],
-}
+  },
+
+  plugins: [
+    new CopyPlugin({
+      patterns: [{ from: "static" }],
+    }),
+
+    new MiniCssExtractPlugin({
+      filename: "[name.css]",
+    }),
+  ],
+};
--- webpack.dev.cjs
+++ webpack.dev.cjs
@@ -1,7 +1,7 @@
-const { merge } = require("webpack-merge")
-const common = require("./webpack.common.cjs")
+const { merge } = require("webpack-merge");
+const common = require("./webpack.common.cjs");
 
 module.exports = merge(common, {
-    mode: "development",
-    devtool: "inline-source-map",
-})
+  mode: "development",
+  devtool: "inline-source-map",
+});
