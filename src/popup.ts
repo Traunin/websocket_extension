108a7ac028cd88ace9c7af4657829bd855650e8e
Traunin <traunin5@gmail.com>
2024-07-18 16:44:38 +0300

    feat: save and display ip and port values

--- src/popup.ts
+++ src/popup.ts
@@ -1,11 +1,29 @@
 import "../styles/popup.css";
-
 const connectButton: Element = document.querySelector(".connect")!;
 const ipField: HTMLInputElement = document.querySelector(".ip")!;
 const portField: HTMLInputElement = document.querySelector(".port")!;
+
+displayDataFromStorage();
 
 connectButton.addEventListener("click", () => {
   let ip = ipField.value;
   let port = portField.value;
   console.log(`ws://${ip}:${port}`);
 });
+
+ipField.addEventListener("keyup", () => {
+  chrome.storage.sync.set({ ip: ipField.value });
+});
+
+portField.addEventListener("keyup", () => {
+  chrome.storage.sync.set({ port: portField.value });
+});
+
+function displayDataFromStorage(): void {
+  chrome.storage.sync.get(["ip"], (storedData) => {
+    ipField.value = storedData["ip"];
+  });
+  chrome.storage.sync.get(["port"], (storedData) => {
+    portField.value = storedData["port"];
+  });
+}
