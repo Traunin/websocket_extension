ade1e7a5903c067a150853f5df08144866094ad1
Traunin <traunin5@gmail.com>
2024-07-24 11:28:40 +0300

    feat: add pause, next/previous song, volume control

--- src/pages/youtube-controller.ts
+++ src/pages/youtube-controller.ts
@@ -1,7 +1,68 @@
+const actionsMap: { [char: string]: () => void } = {
+  f: togglePlay,
+  e: previousSong,
+  d: nextSong,
+  b: decreaseVolume,
+  c: toggleMute,
+  a: increaseVolume,
+};
+
+chrome.runtime.onMessage.addListener((request) => {
+  console.log(request);
+  const action = actionsMap[request];
+
+  if (action) {
+    action();
+  } else {
+    console.error("Action not defined");
+  }
+});
+
 const playButton: HTMLElement = document.querySelector(
   "#left-controls #play-pause-button",
 )!;
 
-chrome.runtime.onMessage.addListener((request, sender, sendResponse) => {
-  playButton.click();
-});
+function togglePlay() {
+  //playButton.click();
+  // space
+  pressKey(" ")
+  console.log("togglePlay");
+}
+
+function previousSong() {
+  //K
+  pressKey("K")
+  console.log("previousSong");
+}
+
+function nextSong() {
+  //J
+  pressKey("J")
+  console.log("nextSong");
+}
+
+function decreaseVolume() {
+  //-
+  pressKey("-")
+  console.log("decreaseVolume");
+}
+
+function toggleMute() {
+  //M
+  pressKey("M")
+  console.log("toggleMute");
+}
+
+function increaseVolume() {
+  //=
+  pressKey("=")
+  console.log("increaseVolume");
+}
+
+function pressKey(key: string) {
+  document.dispatchEvent(
+    new KeyboardEvent("keydown", {
+      key: key,
+    }),
+  );
+}
