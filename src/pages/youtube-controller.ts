47a56fff95eb51fbf3c1c06a2ebd82599f4386eb
Traunin <traunin5@gmail.com>
2024-07-30 02:30:39 +0300

    refactor: remove logging, use controller

--- src/pages/youtube-controller.ts
+++ src/pages/youtube-controller.ts
@@ -1,3 +1,4 @@
+import Controller from "./controller";
 const actionsMap: { [char: string]: () => void } = {
   f: togglePlay,
   e: previousSong,
@@ -7,45 +8,30 @@ const actionsMap: { [char: string]: () => void } = {
   a: increaseVolume,
 };
 
-chrome.runtime.onMessage.addListener((request) => {
-  console.log(request);
-  const action = actionsMap[request];
-
-  if (action) {
-    action();
-  } else {
-    console.error("Action not defined");
-  }
-});
+new Controller(actionsMap);
 
 function togglePlay() {
   pressKey(" ");
-  console.log("togglePlay");
 }
 
 function previousSong() {
   pressKey("K");
-  console.log("previousSong");
 }
 
 function nextSong() {
   pressKey("J");
-  console.log("nextSong");
 }
 
 function decreaseVolume() {
   pressKey("-");
-  console.log("decreaseVolume");
 }
 
 function toggleMute() {
   pressKey("M");
-  console.log("toggleMute");
 }
 
 function increaseVolume() {
   pressKey("=");
-  console.log("increaseVolume");
 }
 
 function pressKey(key: string) {
